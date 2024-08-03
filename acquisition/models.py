b5370f8317faf40e81c9e141abb98e7b3535b2c4
amin-rami <ramimohammadamin@gmail.com>
2024-08-03 07:52:36 +0330

    add snesor data model

--- acquisition/models.py
+++ acquisition/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+
+class SensorData(models.Model):
+    timestamp = models.DateTimeField()
+    sensor_id = models.CharField(max_length=32)
+    value = models.CharField(max_length=2048)
