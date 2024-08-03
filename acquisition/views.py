a7d12026c3131bd1ad35e6052d6679446befa6d5
amin-rami <ramimohammadamin@gmail.com>
2024-08-03 08:40:31 +0330

    add views

--- acquisition/views.py
+++ acquisition/views.py
@@ -1,3 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from acquisition.models import SensorData
+from acquisition.serializers import SensorDataSerializer
+
+
+class SensorDataList(APIView):
+
+    def get(self, request):
+        serializer = SensorDataSerializer(SensorData.objects.all(), many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SensorDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
