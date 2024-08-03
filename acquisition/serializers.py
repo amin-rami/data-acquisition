ec1122ba469ff3b3e00737675baa772b93ec0e30
amin-rami <ramimohammadamin@gmail.com>
2024-08-03 07:57:18 +0330

    add sensor data serializer

---
+++ acquisition/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from acquisition.models import SensorData
+
+
+class SensorDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SensorData
+        fields = '__all__'
