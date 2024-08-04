70243a3e0df754e225a4d00ac48db9be3f7d2fe0
amin-rami <ramimohammadamin@gmail.com>
2024-08-04 13:16:44 +0330

    remove sensor id column

--- acquisition/models.py
+++ acquisition/models.py
@@ -4,5 +4,4 @@ from django.db import models
 
 class SensorData(models.Model):
     timestamp = models.DateTimeField()
-    sensor_id = models.CharField(max_length=32)
     value = models.CharField(max_length=2048)
