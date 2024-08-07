19594e165f2d314cf68088fb50fe8bd1da84780a
amin-rami <ramimohammadamin@gmail.com>
2024-08-07 23:29:59 +0330

    change value type to float and add sensor_id column

--- acquisition/models.py
+++ acquisition/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class SensorData(models.Model):
     timestamp = models.DateTimeField()
-    value = models.CharField(max_length=2048)
+    sensor_id = models.CharField(max_length=64)
+    value = models.FloatField()
